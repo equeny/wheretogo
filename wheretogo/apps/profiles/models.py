92c60a5dcf82aa60e0353553ffd37627e8850046
Vasyl Stanislavchuk <vasyl.stanislavchuk@djangostars.com>
2012-04-22 10:58:40 +0300

    Fixed few bugs

--- wheretogo/apps/profiles/models.py
+++ wheretogo/apps/profiles/models.py
@@ -33,5 +33,6 @@ def new_users_handler(sender, user, response, details, **kwargs):
     profile, c = FacebookProfile.objects.get_or_create(user=user, fid=response['id'])
     profile.oauth_token = response['access_token']
     profile.name = response['name']
+    profile.picture = response['picture']
     profile.save()
     return False
